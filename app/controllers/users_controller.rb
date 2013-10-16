3b9e81261d8f43e34f2ebcb9d9c2a08a949ce595
Chris Syversen <csyversen@gmail.com>
2013-10-16 10:34:37 -0500

    users can only see their own pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :signed_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :signed_in_user 
+  before_action :correct_user
 
   def index
     #@users = User.all
