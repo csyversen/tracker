6bad8ade18dfcfb28198a57d50e5c6ea65589455
Chris Syversen <csyversen@gmail.com>
2013-12-13 16:29:42 -0600

    added a few more tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,11 @@ describe "UserPages" do
 
     describe "page" do
       it { should have_link( "Home/Users", href: user_path(user)) }
+      it { should have_link( "Logout", href: "/logout" ) }
+      it { should have_link( "Products", href: products_path ) }
+      it { should have_field( "product_url") }
+      it { should have_content( "Current Price") }
+      it { should have_selector( "input" ) }
     end
   end
 end
